f89ecd2f3232397d584b668a3ad218195d9a3b47
projectsaathi01 <projectsaathi@gmail.com>
2019-08-22 22:00:37 +0545

    arduino sensor testing sample code

---
+++ mq2/mq2.ino
@@ -0,0 +1,13 @@
+int led_pin = 13;
+int sensor_pin = A0;
+int value = 200;
+void setup() {
+Serial.begin(9600);
+pinMode(led_pin, OUTPUT);
+pinMode(sensor_pin, INPUT);
+}
+void loop() {
+ int sensor_pin  = analogRead(sensor_pin);
+Serial.println(sensor_pin);
+delay(1000);
+}
